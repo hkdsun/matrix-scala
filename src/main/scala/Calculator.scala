c2550ac8f23431fa56fecac7c49e2aee89271690
Hormoz K <hkheradm@gmail.com>
2015-04-03 22:57:53 -0400

    added minicalcs

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -12,12 +12,32 @@ object Calculator {
 
 class Calculator extends Actor{
   val log = Logging(context.system, this)
+	var sum = 0
 	context.watch(sender)
   def receive = {
     case Matrix(rows) => {
-      sender ! rows.flatten.sum
+			rows.map(x => context.actorOf(MiniCalculator.props, name = "miniCalculatorActor") ! x)
     }
+		case x: Int => {
+			sum += x
+			println(sum)
+		}
 		case Terminated(sender) => println("they killed my mom")
     case _ => log.error("Didn't receive a matrix")
   }
+}
+
+object MiniCalculator {
+	def props = Props(new MiniCalculator)
+}
+
+class MiniCalculator extends Actor{
+  val log = Logging(context.system, this)
+	def receive = {
+		case x: Vector[Int] => {
+			sender ! x.sum
+			println(x.sum)
+		}
+		case _ => log.error("received garbage")
+	}
 }
